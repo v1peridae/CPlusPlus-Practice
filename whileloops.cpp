95690090b491bbfb7575a0c19936b9baf21bb930
louisa <82815171+v1peridae@users.noreply.github.com>
2023-09-23 10:24:30 -0700

    Create whileloops.cpp

---
+++ whileloops.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int books = 0;
+  while (books < 6) {
+    cout << "Adding books until 5... " << books << endl;
+    books++;
+  }
+return 0;
+}
